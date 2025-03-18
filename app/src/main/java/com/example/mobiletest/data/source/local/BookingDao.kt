9d39a650ef742bbb4d1092f4e5dd4dd60c9c5ac8
jimmy.ma@bidone.co.nz <jimmy.ma@bidone.co.nz>
2025-03-18 22:02:19 +1300

    Update resource and create local data layer class
    1. booking.json as mock data
    2. BookingDao.kt, BookingDatabase.kt and LocalBooking.kt for Room database

---
+++ app/src/main/java/com/example/mobiletest/data/source/local/BookingDao.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.mobiletest.data.source.local
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface BookingDao {
+
+
+    @Query("SELECT * FROM booking")
+    fun observeAll(): Flow<List<LocalBooking>>
+
+
+    @Query("SELECT * FROM booking WHERE shipToken = :shipToken")
+    fun observeById(shipToken: String): Flow<LocalBooking>
+
+
+    @Query("SELECT * FROM booking")
+    suspend fun getAll(): List<LocalBooking>
+
+
+    @Query("SELECT * FROM booking WHERE shipToken = :shipToken")
+    suspend fun getByShipToken(shipToken: String): LocalBooking?
+
+
+    @Upsert
+    suspend fun upsert(booking: LocalBooking)
+
+
+    @Upsert
+    suspend fun upsertAll(bookings: List<LocalBooking>)
+
+
+    @Query("DELETE FROM booking WHERE shipToken = :shipToken")
+    suspend fun deleteByShipToken(shipToken: String): Int
+
+
+    @Query("DELETE FROM booking")
+    suspend fun deleteAll()
+
+
+}
